de93fb4f180b9a45edc3c07c86a2a50f80ca147c
piquiferrato <juanferrato1994@gmail.com>
2022-04-16 18:19:07 -0300

    combined functions, updated code to be more representative

--- 06_euclian_transformation/main.py
+++ 06_euclian_transformation/main.py
@@ -4,31 +4,35 @@ import cv2;
 image = cv2.imread('image.jpeg')
 
 
-def rotate(image, angle, center=None, scale=1.0):
-    (height, width) = (image.shape[:2])
+def rotate(img, angle, center=None, scale=1.0):
+    (height, width) = (img.shape[:2])
+
     if center is None:
         center = (width/2, height/2)
 
     rotation_matrix = cv2.getRotationMatrix2D(center, angle, scale)
-
-    rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height))
+    rotated_image = cv2.warpAffine(img, rotation_matrix, (width, height))
 
     return rotated_image
 
 
-def traslate(image, x, y):
-    (height, width) = (image.shape[0], image.shape[1])
+def traslate(img, x, y):
+    (height, width) = (img.shape[0], img.shape[1])
 
-    rotation_matrix = np.float32([[1, 0, x],[0, 1, y]]);
-
-    shifted_image = cv2.warpAffine(image, rotation_matrix, (width, height))
+    rotation_matrix = np.float32([[1, 0, x], [0, 1, y]])
+    shifted_image = cv2.warpAffine(img, rotation_matrix, (width, height))
 
     return shifted_image;
 
 
-rotated_image = rotate(image, 90)
+def euclid_transformation(img, angle, x_traslation, y_traslation):
+    rotated = rotate(img, angle);
+    euclid = traslate(rotated, x_traslation, y_traslation)
 
-eculid_transformated_image = traslate(rotated_image, 100, 100)
+    return euclid
+
+
+eculid_transformated_image = euclid_transformation(image, 35, 30, 20)
 
 cv2.imshow('resultado', eculid_transformated_image)
 key = cv2.waitKey(0)
